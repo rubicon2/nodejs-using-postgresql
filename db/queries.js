b5a6e61a901394855f57bce4ba532f40eb320218
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2024-10-04 16:46:31 +0100

    Add error handling for db queries

--- db/queries.js
+++ db/queries.js
@@ -1,18 +1,32 @@
 const pool = require('./pool');
 
 async function getAllUsernames() {
-  const { rows } = await pool.query('SELECT * FROM usernames');
-  return rows;
+  try {
+    const { rows } = await pool.query('SELECT * FROM usernames');
+    return rows;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function insertUsername(username) {
-  // When passing the username in via the second argument's array, pg will take care of escaping the input.
-  await pool.query('INSERT INTO usernames (username) VALUES ($1)', [username]);
+  try {
+    // When passing the username in via the second argument's array, pg will take care of escaping the input.
+    await pool.query('INSERT INTO usernames (username) VALUES ($1)', [
+      username,
+    ]);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 async function deleteUsername(id) {
-  // When passing the username in via the second argument's array, pg will take care of escaping the input.
-  await pool.query('DELETE FROM usernames WHERE id = $1', [id]);
+  try {
+    // When passing the username in via the second argument's array, pg will take care of escaping the input.
+    await pool.query('DELETE FROM usernames WHERE id = $1', [id]);
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 module.exports = {
